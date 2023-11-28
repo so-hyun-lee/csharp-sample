5917d43ca1da7375a8177a5df81f29f8d9755625
Sohyun Lee <sxhxxxlxx@gmail.com>
2023-11-28 11:23:28 +0900

    코드 작성 완료
    --디스크립션 작성 부분--

--- Assets/Scripts/C04/ArithmaticOperators.cs
+++ Assets/Scripts/C04/ArithmaticOperators.cs
@@ -7,12 +7,20 @@ public class ArithmaticOperators : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 111 + 222;
+        Debug.Log($"a:{a}");
+
+        int b = a - 100;
+        Debug.Log($"b:{b}");
+
+        int c = b * 10;
+        Debug.Log($"c:{c}");
+
+        double d = c /6.3;
+        Debug.Log($"c:{c}");
+
+        Debug.Log($"22/7 = {22/7}({22%7})");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
